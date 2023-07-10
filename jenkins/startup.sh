fe35c9cfb72745084fec018a44f3e8c27621afee
MaksimKavalenka <25153828+MaksimKavalenka@users.noreply.github.com>
2023-07-10 22:44:52 +0300

    Jenkins build pod

--- jenkins/startup.sh
+++ jenkins/startup.sh
@@ -1,4 +1,4 @@
 kubectl create namespace jenkins
 helm install jenkins bitnami/jenkins -n jenkins
 kubectl delete service jenkins -n jenkins
-kubectl apply -f ./configurations/nodePort.yaml -n jenkins
+kubectl apply -f ./configurations/node-port.yaml -n jenkins
